5d235ba171084053770f5af76ef41833f52164e4
jeremiah-grayson <grayson4yhwh@gmail.com>
2020-04-01 11:52:39 +0530

    commit 5

--- Day1-BankApp/src/main/java/org/cap/dao/AccountDaoImpl.java
+++ Day1-BankApp/src/main/java/org/cap/dao/AccountDaoImpl.java
@@ -7,13 +7,13 @@ public class AccountDaoImpl implements AccountDao{
 
 	@Override
 	public boolean createAccount(Account account) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public Account findAccountById(int accountNo) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
